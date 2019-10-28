b2937b65387f972ef26d21bb4f3a985d1e599eef
Merttekin <merttekin94@gmail.com>
2019-10-28 21:16:31 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+} from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text>SA</Text>
+    </View>
+  );
+};
