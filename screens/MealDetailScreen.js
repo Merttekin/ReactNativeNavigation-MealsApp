53da453c34a56198d9c1413e93e5b29ec1bf1051
Merttekin <merttekin94@gmail.com>
2019-10-29 20:24:49 +0300

    header button problem

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+
+import { MEALS } from '../data/dummy-data';
+import HeaderButton from '../components/HeaderButton';
 
 const MealDetailScreen = props => {
+    const mealId = props.navigation.getParam('mealId');
+
+    const selectedMeal = MEALS.find(meal => meal.id === mealId);
+
     return(
         <View style={styles.screen}>
-            <Text>MealDetailScreen Screen!</Text>
+            <Text>{selectedMeal.title}</Text>
         </View>
     )
 }
@@ -16,5 +24,24 @@ const styles = StyleSheet.create({
         alignItems: 'center'
     }
 })
+
+MealDetailScreen.navigationOptions = navigationData => {
+    const mealId = navigationData.navigation.getParam('mealId');
+    const selectedMeal = MEALS.find(meal => meal.id === mealId);
+    return{
+        headerTitle: selectedMeal.title,
+        headerRight: ( <Text>FAV!</Text>
+            // <HeaderButtons HeaderButtonComponent={HeaderButton}>
+            //     <Item
+            //         title="Favorite"
+            //         iconName="ios-star"
+            //         onPress={() => {
+
+            //         }}
+            //     />
+            // </HeaderButtons>
+        )
+    }
+}
 
 export default MealDetailScreen;
